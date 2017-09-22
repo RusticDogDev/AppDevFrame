c2d5b12c9187c90b2c66dbc4ec1090bf05560e37
SPEEDO\target <daniel.b.coughlan@mycit.ie>
2017-09-22 12:36:21 +0100

    more stuff

--- TheHitProject/src/main/java/com/thehit/domain/Song.java
+++ TheHitProject/src/main/java/com/thehit/domain/Song.java
@@ -5,27 +5,31 @@ public class Song {
 	private String name;
 	private String lyrics;
 	
-	public Song(String name, String lyrics){
+	public Song(String name, String lyrics)
+	{
 		this.name=name;
 		this.lyrics=lyrics;
-		System.out.println("SONG CONSTRUCTOR: The name of the song is: "+name);
-		System.out.println("SONG CONSTRUCTOR: The lyrics of the song are:"+lyrics);                      
+		System.out.println("SONG CONSTRUCTOR: The name of the song is: " + name);
+		System.out.println("SONG CONSTRUCTOR: The lyrics of the song are:" + lyrics);                      
 	}	
 	public Song(){
 		
 	}	
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getLyrics() {
 		return lyrics;
 	}
+	
 	public void setLyrics(String lyrics) {
 		this.lyrics = lyrics;
-	}	
-	
+	}		
 }
                
